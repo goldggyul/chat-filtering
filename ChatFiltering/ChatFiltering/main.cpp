32badc436500383aea097304496eae8f5ddb036d
ggyul <wakegyul@gmail.com>
2021-09-02 18:11:47 +0900

    Add comments

--- ChatFiltering/ChatFiltering/main.cpp
+++ ChatFiltering/ChatFiltering/main.cpp
@@ -31,7 +31,7 @@ int main()
 std::string Chat::GetFilteredWord(const std::string& filter)
 {
 	std::string filter_word = "";
-	// 필터링 단어가 한글인 경우
+	// 필터링 단어가 한글이므로 2로 나눔
 	for (int i = 0; i < filter.size() / 2; i++)
 	{
 		filter_word.push_back('*');
@@ -44,12 +44,18 @@ std::string Chat::GetSpaceOutput(const std::string& input)
 	const std::string& filter = get_filter();
 	std::string output = input;
 	std::string input_no_space = input;
+
+	// 공백 제거
 	std::string::iterator it = std::remove(input_no_space.begin(), input_no_space.end(), ' ');
 	input_no_space.erase(it, input_no_space.end());
 
+	// 공백 제거한 문자열에서 필터링할 단어 있는 지 확인
 	std::string::size_type no_space_pos = input_no_space.find(filter);
+
+	// 필터링 필요한 경우
 	if (no_space_pos != std::string::npos)
 	{
+		// 공백 제거한 문자열이 아닌, 원래 문자열에서 위치 찾기 위해 공백 수 세기
 		std::vector<int> space_count(input.size(), 0);
 		for (int i = 1; i < input.size(); i++)
 		{
@@ -59,8 +65,12 @@ std::string Chat::GetSpaceOutput(const std::string& input)
 				space_count[i] = space_count[i - 1];
 		}
 
+		// 필터링 단어 첫번째 글자 기준으로 찾기
 		std::string filter_first_letter;
-		filter_first_letter.push_back(filter[0]); filter_first_letter.push_back(filter[1]);
+
+		// 필터링 단어가 한글이므로 2바이트 필요
+		filter_first_letter.push_back(filter[0]);
+		filter_first_letter.push_back(filter[1]);
 		std::string::size_type pos = output.find(filter_first_letter);
 		while (pos != std::string::npos)
 		{
@@ -71,8 +81,9 @@ std::string Chat::GetSpaceOutput(const std::string& input)
 				{
 					output[pos] = '*';
 					output.erase(pos + 1, 1);
-					if (i + 2 < filter.size())
-						pos = output.find(filter[i + 2], pos + 1);
+					if (i + 2 >= filter.size())
+						break;
+					pos = output.find(filter[i + 2], pos + 1);
 				}
 				break;
 			}
@@ -94,6 +105,7 @@ std::string Chat::GetOutput(const std::string& input)
 	}
 	else
 	{
+		// 공백 포함된 상태의 필터링 단어 있는 지 확인
 		output = GetSpaceOutput(input);
 	}
 	return output;
