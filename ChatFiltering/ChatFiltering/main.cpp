8e007da99053f1273679de83073a4012a0c960d2
ggyul <wakegyul@gmail.com>
2021-09-02 15:26:23 +0900

    Implement basic code

--- ChatFiltering/ChatFiltering/main.cpp
+++ ChatFiltering/ChatFiltering/main.cpp
@@ -1,7 +1,56 @@
 #include <iostream>
+#include <string>
+
+class Chat
+{
+private:
+	std::string filter;
+
+public:
+
+	// 초기 설정 
+	Chat() :filter("강아지") {}
+	Chat(std::string filter) :filter(filter) {}
+
+	std::string GetFilteredWord(std::string filter)
+	{
+		std::string filter_word = "";
+		// 필터링 단어가 한글인 경우
+		for (int i = 0; i < filter.size() / 2; i++)
+		{
+			filter_word.push_back('*');
+		}
+		return filter_word;
+	}
+
+	void play()
+	{
+		std::cout << "q 입력 시 종료" << std::endl;
+		while (true)
+		{
+			std::cout << "입력: ";
+			std::string input;
+			getline(std::cin, input);
+			
+			if (input == "q")
+				return;
+
+			std::string::size_type pos = input.find(filter);
+			std::string output = input;
+
+			if (pos != std::string::npos)
+			{
+				output.replace(pos, filter.size(), GetFilteredWord(filter));
+			}
+			std::cout << "출력: " << output << std::endl;
+		}
+	}
+};
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	Chat chat;
+	chat.play();
+
 	return 0;
 }
